b81167cde5f44f12e0c04374da0372da3d761db3
Jonathan-Ma <jma66@hawaii.edu>
2021-10-18 19:02:42 -1000

    Completed islandsnow mockup

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -5,15 +5,6 @@ import 'semantic-ui-css/semantic.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
-import NotFound from '../pages/NotFound';
-import Signin from '../pages/Signin';
-import Signup from '../pages/Signup';
-import Signout from '../pages/Signout';
-import NavBar from '../components/NavBar';
 import IslandSnow from '../pages/IslandSnow';
 import TopMenu from '../components/TopMenu';
 
@@ -26,14 +17,6 @@ class App extends React.Component {
           <TopMenu/>
           <Switch>
             <Route exact path="/" component={IslandSnow}/>
-            <Route path="/signin" component={Signin}/>
-            <Route path="/signup" component={Signup}/>
-            <Route path="/signout" component={Signout}/>
-            <ProtectedRoute path="/list" component={ListStuff}/>
-            <ProtectedRoute path="/add" component={AddStuff}/>
-            <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-            <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
-            <Route component={NotFound}/>
           </Switch>
           <Footer/>
         </div>
